a426b5a16403b2db81301adb5a219c17ba68d540
Arian Glander <glander@me.com>
2021-03-13 00:03:31 +0100

    styling

--- src/views/PageView/PageView.js
+++ src/views/PageView/PageView.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, List, ListItem } from '@material-ui/core';
 import { Section, SectionAlternate } from 'components/organisms';
-import {MDXRenderer} from 'gatsby-plugin-mdx'
+import {MDXRenderer} from 'gatsby-plugin-mdx';
+import {MDXProvider} from '@mdx-js/react';
 
 import PropTypes from 'prop-types';
 import {
@@ -22,6 +23,20 @@ const useStyles = makeStyles((theme) => ({
 		height: '100%',
 		width: '100%',
 	},
+  list: {
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      marginBottom: theme.spacing(4),
+    },
+    fontSize: '1.5em',
+    marginLeft: '40px'
+  },
+  section: {
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      marginBottom: theme.spacing(4),
+    },
+  },
 	sidebarNewsletter: {
 		marginTop: theme.spacing(4),
 		[theme.breakpoints.down('sm')]: {
@@ -35,15 +50,22 @@ const useStyles = makeStyles((theme) => ({
 
 const PageView = (data) => {
 	const classes = useStyles();
-  console.log(data.data.mdx.frontmatter.title);
+  const props = '';
+  const components = {
+    p : (props) => <Typography {...props} component="p" variant="h6" color="textPrimary" className={classes.section} />,
+    h1 : (props) => <Typography {...props} component="h1" variant="h3" color="textPrimary" className={classes.section} />,
+    h2 : (props) => <Typography {...props} component="h2" variant="h4" color="textPrimary" className={classes.section} />,
+    ul : (props) => <ul {...props} className={classes.list} />
+  };
 	return (
 		<div className={classes.root}>
 			<Hero title={data.data.mdx.frontmatter.title} claim={data.data.mdx.frontmatter.claim} description={data.data.mdx.frontmatter.description} />
 			<Section>
 				<Grid container spacing={4}>
 					<Grid item xs={12} md={8}>
+          <MDXProvider components = {components}>
             <MDXRenderer>{data.data.mdx.body}</MDXRenderer>
-            
+          </MDXProvider>
 					</Grid>
 					<Grid item xs={12} md={4}>
 						<SidebarArticles data={sidebarArticles} />
