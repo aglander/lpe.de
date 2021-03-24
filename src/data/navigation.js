a802ed9288764a863b039447292bb7b00ad51994
Arian Glander <glander@me.com>
2021-03-24 17:58:57 +0100

    bild gelöscht und navi angepasst

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -3,81 +3,142 @@ exports.navigation = [
 		id: 'altersvorsorge',
 		title: 'Altersvorsorge',
 	},
-	{
-		id: 'riesterrente',
-		parent: 'altersvorsorge',
-		title: 'Riesterrente',
-		url: '/riesterrente',
-	},
-	{
-		id: 'basisrente',
-		parent: 'altersvorsorge',
-		title: 'Basisrente',
-		url: '/basisrente',
-	},
-	{
-		id: 'rentenversicherung',
-		parent: 'altersvorsorge',
-		title: 'Rentenversicherung',
-		url: '/rentenversicherung',
-	},
-	{
-		id: 'fondsgebundene-rentenversicherung',
-		parent: 'altersvorsorge',
-		title: 'Fondsgebundene Rentenversicherung',
-		url: '/fondsgebundene-rentenversicherung',
-	},
-	{
-		id: 'kapitallebensversicherung',
-		parent: 'altersvorsorge',
-		title: 'Kapitallebensversicherung',
-		url: '/kapitallebensversicherung',
-	},
-	{
-		id: 'fondsgebundene-lebensversicherung',
-		parent: 'altersvorsorge',
-		title: 'Fondsgebundene Lebensversicherung',
-		url: '/fondsgebundene-lebensversicherung',
-	},
-	{
-		id: 'risikolebensversicherung',
-		parent: 'altersvorsorge',
-		title: 'Risikolebensversicherung',
-		url: '/risikolebensversicherung',
-	},
-	{
-		id: 'berufsunfaehigkeitsversicherung',
-		parent: 'altersvorsorge',
-		title: 'Berufsunfähigkeitsversicherung',
-		url: '/berufsunfaehigkeitsversicherung',
-	},
-	{
-		id: 'schwere-krankheiten-versicherung',
-		parent: 'altersvorsorge',
-		title: 'Schwere Krankheiten Versicherung',
-		url: '/schwere-krankheiten-versicherung',
-	},
+		{
+			id: 'riesterrente',
+			parent: 'altersvorsorge',
+			title: 'Riester-Rente',
+			url: '/riesterrente',
+		},
+		{
+			id: 'basisrente',
+			parent: 'altersvorsorge',
+			title: 'Basis-Rente',
+			url: '/basisrente',
+		},
+		{
+			id: 'rentenversicherung',
+			parent: 'altersvorsorge',
+			title: 'Rentenversicherung',
+			url: '/rentenversicherung',
+		},
+		{
+			id: 'lebensversicherung',
+			parent: 'altersvorsorge',
+			title: 'Lebensversicherung',
+			url: '/lebensversicherung',
+		},
 	{
 		id: 'versicherungen',
 		title: 'Versicherungen',
 	},
-	{
-		id: 'risikovorsorge',
-		parent: 'versicherungen',
-		title: 'Risikovorsorge',
-	},
-	{
-		id: 'versicherung-risikolebensversicherung',
-		parent: 'risikovorsorge',
-		title: 'Risikolebensversicherung',
-		url: '/risikolebensversicherung',
-	},
-	{
-		id: 'versicherung-berufsunfaehigkeitsversicherung',
-		parent: 'risikovorsorge',
-		title: 'Berufsunfähigkeitsversicherung',
-		url: '/berufsunfaehigkeitsversicherung',
-	},
+		{
+			id: 'risikovorsorge-arbeitskraftabsicherung',
+			parent: 'versicherungen',
+			title: 'Risikovorsorge & Arbeitskraftabsicherung',
+		},
+			{
+				id: 'test1',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Test 1',
+				url: '/test'
+			},
+		{
+			id: 'haftpflicht-haus',
+			parent: 'versicherungen',
+			title: 'Haftpflicht & Haus',
+		},
+			{
+				id: 'test2',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Test 2',
+				url: '/test'
+			},
+		{
+			id: 'rechtsschutz-reisen',
+			parent: 'versicherungen',
+			title: 'Rechtsschutz & Reisen',
+		},
+			{
+				id: 'test3',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Test 3',
+				url: '/test'
+			},
+		{
+			id: 'auto-fahrzeuge',
+			parent: 'versicherungen',
+			title: 'Auto & Fahrzeuge',
+		},
+			{
+				id: 'test4',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Test 4',
+				url: '/test'
+			},
+		{
+			id: 'krankenzusatzversicherungen',
+			parent: 'versicherungen',
+			title: 'Risikovorsorge & Arbeitskraftabsicherung2',
+		},
+			{
+				id: 'test5',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Test 5',
+				url: '/test'
+			},
+		{
+			id: 'risikovorsorge-arbeitskraftabsicherung',
+			parent: 'versicherungen',
+			title: 'Risikovorsorge & Arbeitskraftabsicherung3',
+		},
+			{
+				id: 'risikolebensversicherung',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Risikolebensversicherung',
+				url: '/risikolebensversicherung',
+			},
+			{
+				id: 'berufsunfaehigkeitsversicherung',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Berufsunfähigkeitsversicherung',
+				url: '/berufsunfaehigkeitsversicherung',
+			},
+			{
+				id: 'unfallversicherung',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Unfallversicherung',
+				url: '/unfallversicherung',
+			},
+			{
+				id: 'schwere-krankheiten',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Schwere Krankheiten',
+				url: '/schwere-krankheiten',
+			},
+			{
+				id: 'grundfaehigkeitsabsicherung',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'grundfaehigkeitsabsicherung',
+				url: '/grundfaehigkeitsabsicherungg',
+			},
+			{
+				id: 'krankentagegeld',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Krankentagegeld',
+				url: '/krankentagegeld',
+			},	
+			{
+				id: 'pflegeversicherung',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Pflegeversicherung',
+				url: '/pflegeversicherung',
+			},
+			{
+				id: 'sterbegeldversicherung',
+				parent: 'risikovorsorge-arbeitskraftabsicherung',
+				title: 'Sterbegeldversicherung',
+				url: '/sterbegeldversicherung',
+			},
 	{
 		id: 'finanzierungen',
 		title: 'Finanzierungen',
