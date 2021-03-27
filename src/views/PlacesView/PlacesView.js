dfb78dd26c78e2c3c5efe70273cbc2f6fbd55ea3
Arian Glander <glander@me.com>
2021-03-27 10:26:08 +0100

    erste formatierungen des Ortsverzeichnis

--- src/views/PlacesView/PlacesView.js
+++ src/views/PlacesView/PlacesView.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useStaticQuery, graphql, Link } from 'gatsby';
+import { Section, ContactPanel } from 'components/organisms';
+import { Grid, Typography, List, ListItem } from '@material-ui/core';
 
 const { places } = require('../../data/places');
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		height: '100%',
+		width: '100%',
+	},
+	section: {
+		marginBottom: theme.spacing(2),
+		[theme.breakpoints.up('md')]: {
+			marginBottom: theme.spacing(4),
+		},
+	},
+}));
 
 const SeoPages = (placeSlug) => {
 	const data = useStaticQuery(graphql`
@@ -23,24 +39,53 @@ const SeoPages = (placeSlug) => {
 		} = node;
 		const url = `/${slug}-${placeSlug.placeSlug}`;
 		return (
-			<li>
+			<ListItem>
 				<Link to={url}>{title}</Link>
-			</li>
+			</ListItem>
 		);
 	});
-	return <ul>{seoPages}</ul>;
+	return <List>{seoPages}</List>;
 };
 
 const PlacesView = (data) => {
+	const classes = useStyles();
 	const placesList = places.map((place) => {
 		return (
 			<div>
-				<h1>{place.title}</h1>
-				<SeoPages placeSlug = {place.slug}/>
+				<Typography
+					component="h2"
+					variant="h4"
+					color="textPrimary"
+					className={classes.section}
+				>
+					{place.title}
+				</Typography>
+				<SeoPages placeSlug={place.slug} />
 			</div>
 		);
 	});
-	return <div>{placesList}</div>;
+	return (
+		<div className={classes.root}>
+			<Section>
+				<Grid container spacing={4}>
+					<Grid item xs={12} md={8}>
+						<Typography
+							component="h1"
+							variant="h3"
+							color="textPrimary"
+							className={classes.section}
+						>
+							Ortsverzeichnis
+						</Typography>
+						{placesList}
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<ContactPanel />
+					</Grid>
+				</Grid>
+			</Section>
+		</div>
+	);
 };
 
 PlacesView.propTypes = {};
