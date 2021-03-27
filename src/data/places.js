9863496890692ef361e08088bdfda70ae029532c
Arian Glander <glander@me.com>
2021-03-27 14:35:14 +0100

    Ortsdatenbank hinzugefügt

--- src/data/places.js
+++ src/data/places.js
@@ -1,5 +1,206 @@
 exports.places = [
 
+
+    {
+        slug: '12527-berlin-schmoeckwitz',
+        title: '12527 Berlin-Schmöckwitz',
+    },
+    {
+        slug: '12557-berlin-wendenschloss',
+        title: '12557 Berlin-Wendenschloss',
+    },
+    {
+        slug: '12559-berlin-mueggelheim',
+        title: '12559 Berlin-Müggelheim',
+    },
+    {
+        slug: '12587-berlin-elsengrund',
+        title: '12587 Berlin-Elsengrund',
+    },
+    {
+        slug: '12589-berlin-rahnsdorf',
+        title: '12589 Berlin-Rahnsdorf',
+    },
+    {
+        slug: '12589-berlin-wilhelmshagen',
+        title: '12589 Berlin-Wilhelmshagen',
+    },
+    {
+        slug: '12589-hessenwinkel',
+        title: '12589 Hessenwinkel',
+    },
+    {
+        slug: '12623-berlin-mahlsdorf',
+        title: '12623 Berlin-Mahlsdorf',
+    },
+    {
+        slug: '15344-strausberg',
+        title: '15344 Strausberg',
+    },
+    {
+        slug: '15345-altlandsberg',
+        title: '15345 Altlandsberg',
+    },
+    {
+        slug: '15345-bruchmühle',
+        title: '15345 Bruchmühle',
+    },
+    {
+        slug: '15345-eggersdorf-petershagen',
+        title: '15345 Eggersdorf-Petershagen',
+    },
+    {
+        slug: '15345 garzau-garzin',
+        title: '15345 Garzau-Garzin',
+    },
+    {
+        slug: '15345-heidekrug',
+        title: '15345 Heidekrug',
+    },
+    {
+        slug: '15345-lichtenow',
+        title: '15345 Lichtenow',
+    },
+    {
+        slug: '15345-rehfelde',
+        title: '15345 Rehfelde',
+    },
+    {
+        slug: '15345-wegendorf',
+        title: '15345 Wegendorf',
+    },
+    {
+        slug: '15345-werder',
+        title: '15345 Werder',
+    },
+    {
+        slug: '15345-zinndorf',
+        title: '15345 Zinndorf',
+    },
+    {
+        slug: '15366-birkenstein',
+        title: '15366 Birkenstein',
+    },
+    {
+        slug: '15366-hoenow',
+        title: '15366 Hönow',
+    },
+    {
+        slug: '15366-hoppegarten',
+        title: '15366 Hoppegarten',
+    },
+    {
+        slug: '15366-muenchehofe',
+        title: '15366 Münchehofe',
+    },
+    {
+        slug: '15366-neuenhagen-bei-berlin',
+        title: '15366 Neuenhagen bei Berlin',
+    },
+    {
+        slug: '15366-waldesruh',
+        title: '15366 Waldesruh',
+    },
+    {
+        slug: '15370-fredersdorf-vogelsdorf',
+        title: '15370 Fredersdorf-Vogelsdorf',
+    },
+    {
+        slug: '15370-petershagen-eggersdorf',
+        title: '15370 Petershagen-Eggersdorf',
+    },
+    {
+        slug: '15374-muencheberg',
+        title: '15374 Müncheberg',
+    },
+    {
+        slug: '15377-buckow',
+        title: '15377 Buckow',
+    },
+    {
+        slug: '15377-waldsieversdorf',
+        title: '15377 Waldsieversdorf',
+    },
+    {
+        slug: '15378-hennickendorf',
+        title: '15378 Hennickendorf',
+    },
+    {
+        slug: '15378-herzfelde',
+        title: '15378 Herzfelde',
+    },
+    {
+        slug: '15517-fuerstenwalde',
+        title: '15517 Fürstenwalde',
+    },
+    {
+        slug: '15528-hartmannsdorf',
+        title: '15528 Hartmannsdorf',
+    },
+    {
+        slug: '15528-spreenhagen',
+        title: '15528 Spreenhagen',
+    },
+    {
+        slug: '15537-alt-buchhorst',
+        title: '15537 Alt Buchhorst',
+    },
+    {
+        slug: '15537-burig',
+        title: '15537 Burig',
+    },
+    {
+        slug: '15537-erkner',
+        title: '15537 Erkner',
+    },
+    {
+        slug: '15537-fangschleuse',
+        title: '15537 Fangschleuse',
+    },
+    {
+        slug: '15537-freienbrink',
+        title: '15537 Freienbrink',
+    },
+    {
+        slug: '15537-gosen',
+        title: '15537 Gosen',
+    },
+    {
+        slug: '15537-gruenheide',
+        title: '15537 Grünheide (Mark)',
+    },
+    {
+        slug: '15537-hangelsberg',
+        title: '15537 Hangelsberg',
+    },
+    {
+        slug: '15537-kagel',
+        title: '15537 Kagel',
+    },
+    {
+        slug: '15537-karutzhoehe',
+        title: '15537 Karutzhöhe',
+    },
+    {
+        slug: '15537-kienbaum',
+        title: '15537 Kienbaum',
+    },
+    {
+        slug: '15537-moellensee',
+        title: '15537 Möllensee',
+    },
+    {
+        slug: '15537-moenchwinkel',
+        title: '15537 Mönchwinkel',
+    },
+    {
+        slug: '15537-neu-zittau',
+        title: '15537 Neu Zittau',
+    },
+    {
+        slug: '15537-spreewerder',
+        title: '15537 Spreewerder',
+    },
     {
         slug: '15562-ruedersdorf-bei-berlin',
         title: '15562 Rüdersdorf bei Berlin',
@@ -12,6 +213,114 @@ exports.places = [
     {
         slug: '15569-woltersdorf',
         title: '15569 Woltersdorf',
+    },
+    {
+        slug: '15711-diepensee',
+        title: '15711 Diepensee',
+    },
+    {
+        slug: '15711-koenigs-wusterhausen',
+        title: '15711 Königs Wusterhausen',
+    },
+    {
+        slug: '15711-zeesen',
+        title: '15711 Zeesen',
+    },
+    {
+        slug: '15712-kablow',
+        title: '15712 Kablow',
+    },
+    {
+        slug: '15712-zernsdorf',
+        title: '15712 Zernsdorf',
+    },
+    {
+        slug: '15751-niederlehme',
+        title: '15751 Niederlehme',
+    },
+    {
+        slug: '15713-wernsdorf',
+        title: '15713 Wernsdorf',
+    },
+    {
+        slug: '15713-ziegenhals',
+        title: '15713 Ziegenhals',
+    },
+    {
+        slug: '15732-eichwalde',
+        title: '15732 Eichwalde',
+    },
+    {
+        slug: '15732-schulzendorf',
+        title: '15732 Schulzendorf',
+    },
+    {
+        slug: '15738-zeuthen',
+        title: '15738 Zeuthen',
+    },
+    {
+        slug: '15738-zeuthen-miersdorf',
+        title: '15738 Zeuthen-Miersdorf',
+    },
+    {
+        slug: '15745-wildau',
+        title: '15745 Wildau',
+    },
+    {
+        slug: '15754-friedersdorf',
+        title: '15754 Friedersdorf',
+    },
+    {
+        slug: '15754-friedrichshof',
+        title: '15754 Friedrichshof',
+    },
+    {
+        slug: '16356-ahrensfelde',
+        title: '16356 Ahrensfelde',
+    },
+    {
+        slug: '16356-blumberg',
+        title: '16356 Blumberg',
+    },
+    {
+        slug: '16356-loehme',
+        title: '16356 Löhme',
+    },
+    {
+        slug: '16356-mehrow',
+        title: '16356 Mehrow',
+    },
+    {
+        slug: '16356-seefeld',
+        title: '16356 Seefeld',
+    },
+    {
+        slug: '16356-werneuchen',
+        title: '16356 Werneuchen',
+    },
+    {
+        slug: 'berlin-friedrichshagen',
+        title: 'Berlin-Friedrichshagen',
+    },
+    {
+        slug: 'berlin-gruenau',
+        title: 'Berlin-Grünau',
+    },
+    {
+        slug: 'berlin-hellersdorf',
+        title: 'Berlin-Hellersdorf',
+    },
+    {
+        slug: 'berlin-kaulsdorf',
+        title: 'Berlin-Kaulsdorf',
+    },
+    {
+        slug: 'berlin-koepenick',
+        title: 'Berlin-Köpenick',
+    },
+    {
+        slug: 'berlin-marzahn',
+        title: 'Berlin-Marzahn',
     },
     
     
