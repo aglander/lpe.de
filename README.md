16ea82e01a58e40f40d10c640374e0d262201a84
LPE99 <81194347+LPE99@users.noreply.github.com>
2021-04-16 23:41:10 +0200

    formatierungen

--- README.md
+++ README.md
@@ -15,29 +15,12 @@
 - contact button rechts
 - vergleichsrechner zurück links
 
-
-- Startseite:
-    -   Test-Wording: 
-        -   Lars-Peter Eckhardt, Ihr unabhängiger ... (4 Begriffe in grün)
-        -   Willkommen bei Lars-Peter Eckhardt, Ihrem unabhängigen ... (4 Begriffe in grün)
-    -   Test-Optik:
-        -   Logo und / Bild(er) | 2 x ähnliches Bild von mir sieht komisch aus!
-    -   Inhalt: Siehe Email v. 11.03.2021
-    -   ProvenExpert-Widget
-
-- Box   (2 x 3 oder 2 x 4 Auszeichnungssiegel [WhoFinance und ProvenExpert])
-
-- Bestätigung zum Erhalt der Erstinformation (Kästchen zum anhaken, dass Erstinfo heruntergeladen wurde)
 - Einwilligung zu Cookies & Daten (vielleicht über https://www.consentmanager.net/ | Alternativen?) -> idealerweise kein Cookie Consent nötig
 
-- SEO-SEITEN
-    -   Immer wiederkehrende Sachen optisch vereinheitlichen (kleinere Schriftarten, als Auflistung mit Komma) > SEO-PLZ-Versicherungsmakler >>> "Als V-Makler haben wir im Angebot" gleich (in Form von Boxen) wie "Mit diesen Partner arbeiten wir zusammen" 
-    -   Box mit Kundenstimmen (wie auf der Startseite)
-        -   dafür das ProvenExpert-Widget in voller Breite!
-
-- Optik:
-    -   Trennungsstriche / Boxen zwischen den Themen und Texten
-    -   farbige Textausschnitte
+- Übersichtsseiten Orte, Versicherungen, Altersvorsorge und Finanzierungen
+- Modul für Navigation
+- erstinformation in den footer
+- FB, LinkedIn, XING, Youtube
 
 # Todos Lars
 
--- src/components/organisms/AwardBox/AwardBox.js
+++ src/components/organisms/AwardBox/AwardBox.js
@@ -1,140 +1,146 @@
-import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
-import { Typography, List, ListItem } from '@material-ui/core';
-import { Link } from 'gatsby';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { Typography, List, ListItem } from "@material-ui/core";
+import { Link } from "gatsby";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-	award: {
-		display: 'inline-block',
-		width: 'auto',
-		paddingRight: '10px',
-	},
-	root: {
-		borderColor: theme.palette.alternate.dark,
-		borderWidth: '1px',
-		borderStyle: 'solid',
-		borderRadius: theme.spacing(2),
-		background: theme.palette.alternate.main,
-		marginBottom: '30px',
-		padding: '40px',
-		textAlign: 'center'
-	},
+  award: {
+    display: "inline-block",
+    width: "auto",
+    paddingRight: "10px",
+  },
+  root: {
+    borderColor: theme.palette.alternate.dark,
+    borderWidth: "1px",
+    borderStyle: "solid",
+    borderRadius: theme.spacing(2),
+    background: theme.palette.alternate.main,
+    marginBottom: "30px",
+    padding: "40px",
+    textAlign: "center",
+  },
+  inner: {
+	  maxWidth: '820px',
+	  margin: '0 auto',
+  }
 }));
 
 const AwardBox = () => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<div className={classes.root}>
-			<Typography component="p" variant="h6">
-				Seit 2011 wird LPE | Lars-Peter Eckhardt
-				ununterbrochen durch die unabhängige Bewertungsplattform{' '}
-				<Typography
-					component="a"
-					variant="h6"
-					color="primary"
-					href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
-					target="_blank"
-				>
-					WhoFinance.de 
-				</Typography>{' '}
-				als einer der Top Berater in Deutschland ausgezeichnet.
-			</Typography>
-			<List>
-				<ListItem disableGutters="true" className={classes.award}>
-					<Link
-						href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
-						target="_blank"
-					>
-						<StaticImage
-							src="../../../data/assets/Auszeichnung_aktuell_1.png"
-							alt="Auszeichnung 1"
-							placeholder="blurred"
-							layout="fixed"
-							width={200}
-							height={92}
-						/>
-					</Link>
-				</ListItem>
-				<ListItem disableGutters="true" className={classes.award}>
-					<Link
-						href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
-						target="_blank"
-					>
-						<StaticImage
-							src="../../../data/assets/Auszeichnung_aktuell_2.png"
-							alt="Auszeichnung 2"
-							placeholder="blurred"
-							layout="fixed"
-							width={200}
-							height={92}
-						/>
-					</Link>
-				</ListItem>
-				<ListItem disableGutters="true" className={classes.award}>
-					<Link
-						href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
-						target="_blank"
-					>
-						<StaticImage
-							src="../../../data/assets/Auszeichnung_aktuell_3.png"
-							alt="Auszeichnung 3"
-							placeholder="blurred"
-							layout="fixed"
-							width={200}
-							height={92}
-						/>
-					</Link>
-				</ListItem>
-				<ListItem disableGutters="true" className={classes.award}>
-					<Link
-						href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
-						target="_blank"
-					>
-						<StaticImage
-							src="../../../data/assets/Auszeichnung_aktuell_4.png"
-							alt="Auszeichnung 4"
-							placeholder="blurred"
-							layout="fixed"
-							width={200}
-							height={92}
-						/>
-					</Link>
-				</ListItem>
-				<ListItem disableGutters="true" className={classes.award}>
-					<Link
-						href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
-						target="_blank"
-					>
-						<StaticImage
-							src="../../../data/assets/Auszeichnung_aktuell_5.png"
-							alt="Auszeichnung 5"
-							placeholder="blurred"
-							layout="fixed"
-							width={200}
-							height={92}
-						/>
-					</Link>
-				</ListItem>
-				<ListItem disableGutters="true" className={classes.award}>
-					<Link
-						href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
-						target="_blank"
-					>
-						<StaticImage
-							src="../../../data/assets/Auszeichnung_aktuell_6.png"
-							alt="Auszeichnung 6"
-							placeholder="blurred"
-							layout="fixed"
-							width={200}
-							height={92}
-						/>
-					</Link>
-				</ListItem>
-			</List>
-		</div>
-	);
+  return (
+    <div className={classes.root}>
+      <div className={classes.inner}>
+        <Typography component="p" variant="h6">
+          Seit 2011 wird LPE | Lars-Peter Eckhardt ununterbrochen durch die
+          unabhängige Bewertungsplattform{" "}
+          <Typography
+            component="a"
+            variant="h6"
+            color="primary"
+            href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
+            target="_blank"
+          >
+            WhoFinance.de
+          </Typography>{" "}
+          als einer der Top Berater in Deutschland ausgezeichnet.
+        </Typography>
+        <List>
+          <ListItem disableGutters="true" className={classes.award}>
+            <Link
+              href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
+              target="_blank"
+            >
+              <StaticImage
+                src="../../../data/assets/Auszeichnung_aktuell_1.png"
+                alt="Auszeichnung 1"
+                placeholder="blurred"
+                layout="fixed"
+                width={200}
+                height={92}
+              />
+            </Link>
+          </ListItem>
+          <ListItem disableGutters="true" className={classes.award}>
+            <Link
+              href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
+              target="_blank"
+            >
+              <StaticImage
+                src="../../../data/assets/Auszeichnung_aktuell_2.png"
+                alt="Auszeichnung 2"
+                placeholder="blurred"
+                layout="fixed"
+                width={200}
+                height={92}
+              />
+            </Link>
+          </ListItem>
+          <ListItem disableGutters="true" className={classes.award}>
+            <Link
+              href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
+              target="_blank"
+            >
+              <StaticImage
+                src="../../../data/assets/Auszeichnung_aktuell_3.png"
+                alt="Auszeichnung 3"
+                placeholder="blurred"
+                layout="fixed"
+                width={200}
+                height={92}
+              />
+            </Link>
+          </ListItem>
+          <ListItem disableGutters="true" className={classes.award}>
+            <Link
+              href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
+              target="_blank"
+            >
+              <StaticImage
+                src="../../../data/assets/Auszeichnung_aktuell_4.png"
+                alt="Auszeichnung 4"
+                placeholder="blurred"
+                layout="fixed"
+                width={200}
+                height={92}
+              />
+            </Link>
+          </ListItem>
+          <ListItem disableGutters="true" className={classes.award}>
+            <Link
+              href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
+              target="_blank"
+            >
+              <StaticImage
+                src="../../../data/assets/Auszeichnung_aktuell_5.png"
+                alt="Auszeichnung 5"
+                placeholder="blurred"
+                layout="fixed"
+                width={200}
+                height={92}
+              />
+            </Link>
+          </ListItem>
+          <ListItem disableGutters="true" className={classes.award}>
+            <Link
+              href="https://www.whofinance.de/berater/lars-peter-eckhardt/"
+              target="_blank"
+            >
+              <StaticImage
+                src="../../../data/assets/Auszeichnung_aktuell_6.png"
+                alt="Auszeichnung 6"
+                placeholder="blurred"
+                layout="fixed"
+                width={200}
+                height={92}
+              />
+            </Link>
+          </ListItem>
+        </List>
+      </div>
+    </div>
+  );
 };
 
 export default AwardBox;
