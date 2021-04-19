7bd57a4b6f66c4d7dd0567d1d001a4b49a41346d
Arian Glander <glander@me.com>
2021-04-19 22:01:45 +0200

    place

--- src/components/organisms/Place/Place.js
+++ src/components/organisms/Place/Place.js
@@ -3,12 +3,8 @@ import PropTypes from 'prop-types';
 
 const Place = (props) => {
 	const { placeData, ...rest } = props;
-	let placeName = placeData.title;
-	if (placeData.placeNames) {
-		const placeNames = placeData.placeNames;
-		placeName = placeNames[Math.floor(Math.random() * placeNames.length)];
-	}
-	return <span>{placeName} </span>;
+
+	return <span>{placeData.short} </span>;
 };
 
 export default Place;
