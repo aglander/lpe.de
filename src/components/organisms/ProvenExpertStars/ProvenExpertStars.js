ca934ac8d8f678dfa803037f49801d1f789284d0
Arian Glander <glander@me.com>
2021-05-12 11:23:01 +0200

    provenexpert stars

--- src/components/organisms/ProvenExpertStars/ProvenExpertStars.js
+++ src/components/organisms/ProvenExpertStars/ProvenExpertStars.js
@@ -1,7 +1,33 @@
 import React from 'react';
-import provenExpertData from '../../../../static/provenexpert.json';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const ProvenExpertStars = () => {
+	let provenExpertData = {
+		'@context': 'https://schema.org/',
+		'@type': 'Product',
+		name: 'Lars-Peter Eckhardt',
+		description: 'Ihr Makler für: Altervorsorge | Versicherungen | Finanzierungen | Immobilien',
+		aggregateRating: {
+			'@type': 'AggregateRating',
+			ratingValue: 0,
+			ratingCount: 0,
+			bestRating: 5,
+		},
+	};
+
+	const { dataJson: dynamicValues } = useStaticQuery(graphql`
+		{
+			dataJson {
+				ratingValue
+				reviewCount
+				aggregateRating
+			}
+		}
+	`);
+
+	provenExpertData.aggregateRating.ratingValue = dynamicValues.ratingValue;
+	provenExpertData.aggregateRating.ratingCount = dynamicValues.reviewCount;
+
 	return (
 		<script
 			type="application/ld+json"
