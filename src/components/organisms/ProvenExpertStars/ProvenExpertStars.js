fca3b0701def7e4db438c9d6dc32360ae4ac76c1
Arian Glander <glander@me.com>
2021-05-14 20:43:27 +0200

    provenexpert stars

--- src/components/organisms/ProvenExpertStars/ProvenExpertStars.js
+++ src/components/organisms/ProvenExpertStars/ProvenExpertStars.js
@@ -4,15 +4,22 @@ import { useStaticQuery, graphql } from 'gatsby';
 const ProvenExpertStars = () => {
 	let provenExpertData = {
 		'@context': 'https://schema.org/',
-		'@type': 'Product',
-		name: 'Lars-Peter Eckhardt',
-		description: 'Ihr Makler für: Altervorsorge | Versicherungen | Finanzierungen | Immobilien',
-		aggregateRating: {
-			'@type': 'AggregateRating',
-			ratingValue: 0,
-			ratingCount: 0,
-			bestRating: 5,
+		'@type': 'AggregateRating',
+		itemReviewed: {
+			'@type': 'Product',
+			name: 'LPE | Lars-Peter Eckhardt',
+			description:
+				'Ihr Makler für: Altervorsorge | Versicherungen | Finanzierungen | Immobilien',
+			brand: '',
+			sku: '',
+			mpn: '',
+			image:
+				'https://images.provenexpert.com/75/85/c0fdf5733a2767d5d7634b36fd88/lars-peter-eckhardt_full_1524080663.jpg',
+			url: 'https://www.provenexpert.com/lars-peter-eckhardt/',
 		},
+		ratingValue: 0,
+		ratingCount: 0,
+		bestRating: 5,
 	};
 
 	const { dataJson: dynamicValues } = useStaticQuery(graphql`
