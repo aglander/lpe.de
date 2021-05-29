5cfd3f6feb2218e518d5d198b8916241eef4dcb5
Arian Glander <glander@me.com>
2021-05-29 21:47:59 +0200

    delete old files

--- src/components/organisms/Place/Place.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Place = (props) => {
-	const { placeData, long } = props;
-
-	return <span>{long ? placeData.title : placeData.short} </span>;
-};
-
-export default Place;
