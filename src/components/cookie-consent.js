22a2471e567f98a55fb877aea1b8ad1738836094
Arian Glander <glander@me.com>
2021-05-30 21:35:51 +0200

    debug 1000

--- src/components/cookie-consent.js
+++ src/components/cookie-consent.js
@@ -5,14 +5,16 @@ import Button from "./button"
 import Cookies from "js-cookie"
 
 const CookieConsent = () => {
-  const [open, setOpen] = useState(!Cookies.get("gatsby-gdpr-google-analytics"))
+  const [open, setOpen] = useState(false)
   const [count, setCount] = useState(0)
+
+  setOpen(!Cookies.get("gatsby-gdpr-google-analytics"))
 
   const enableTracking = doIt => {
     if (doIt) {
       Cookies.set("gatsby-gdpr-google-analytics", "true", { expires: 365 })
     } else {
-      Cookies.set("gatsby-gdpr-google-analytics", "false", { expires: 7 })
+      Cookies.set("gatsby-gdpr-google-analytics", "false", { expires: 365 })
     }
   }
 
@@ -55,9 +57,9 @@ const CookieConsent = () => {
             <Button onClick={accept}>Akzeptieren</Button>
             <ul>
               <li>{count}</li>
+              <li>{open}</li>
               <li>{Cookies.get("gatsby-gdpr-google-analytics")}</li>
               <li>{JSON.stringify(Cookies.get())}</li>
-              <li>{open.toString()}</li>
             </ul>
           </div>
         </Box>
