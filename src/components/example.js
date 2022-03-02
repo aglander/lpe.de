f80a5325a91f2f163369f53bf68b1b0495ce6c37
Arian Glander <glander@me.com>
2022-03-02 14:07:29 +0100

    text smaller

--- src/components/example.js
+++ src/components/example.js
@@ -4,7 +4,7 @@ const Example = ({ title, children, number }) => (
   <div class="inline-block sm:w-1/2 lg:w-1/3 p-5 align-top text-left">
     <span class="border-green border-2 rounded-2xl bg-white px-4 py-2 text-green text-2xl inline-block mb-2">{number}</span>
     <h4 class="text-xl mb-2 font-bold hyphens">{title}</h4>
-    <p class="hyphens">{children}</p>
+    <p class="hyphens text-base">{children}</p>
   </div>
 )
 
--- src/components/mdx.js
+++ src/components/mdx.js
@@ -8,15 +8,15 @@ import { Link } from "gatsby"
 const Mdx = ({ components, children }) => {
   const mdxComponents = {
     hr: () => <Divider class="mb-7" />,
-    p: props => <p class="text-base lg:text-xl mb-6 lg:leading-9" {...props} />,
+    p: props => <p class="text-base lg:text-lg mb-6 lg:leading-7" {...props} />,
     h1: props => (
-      <h2 class="text-4xl lg:text-5xl mb-10 hyphens" {...props}>
+      <h2 class="text-3xl lg:text-4xl mb-10 hyphens" {...props}>
         {props.children}
       </h2>
     ),
     h2: props => (
       <h3
-        class="text-2xl lg:text-4xl mt-12 mb-8 border-b border-divider pb-4 hyphens"
+        class="text-xl lg:text-3xl mt-12 mb-8 border-b border-divider pb-4 hyphens"
         {...props}
       >
         {props.children}
