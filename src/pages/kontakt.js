61a2145fbf41408548b00c323def574d05f9940a
LPE99 <81194347+LPE99@users.noreply.github.com>
2025-08-02 08:39:58 +0000

    Kontaktseite Zentral / Postanschrift

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -53,7 +53,7 @@ const KontaktPage = ({ location }) => {
               Webseite: <a href="https://www.lpe.de">www.LPE.de</a><br /><br />
             </p>
             <p>
-              <b>Postanschrift & Sitz Müncheberg:</b><br />
+              <b>Zentrale / Postanschrift Müncheberg:</b><br />
               Wasserstraße 2<br />
               15374 Müncheberg<br />
               Telefon: (033432) 89101<br />
