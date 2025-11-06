a6353dfc975044a3fcfbf8e237a1304bedf82fc4
Arian Glander <Arian Glander>
2025-11-06 16:36:53 +0100

    rating google fix

--- src/components/proven-expert-stars.js
+++ src/components/proven-expert-stars.js
@@ -2,52 +2,46 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const ProvenExpertStars = () => {
-  let provenExpertData = {
-    "@context": "https://schema.org/",
-    "@type": "Product",
-    name: "LPE | Lars-Peter Eckhardt",
-    description:
-      "Ihr Makler für: Altervorsorge | Versicherungen | Finanzierungen | Immobilien",
-    brand: "LPE",
-    sku: "LPE",
-    mpn: "LPE",
-    image:
-      "https://images.provenexpert.com/75/85/c0fdf5733a2767d5d7634b36fd88/lars-peter-eckhardt_full_1524080663.jpg",
-    url: "https://www.provenexpert.com/lars-peter-eckhardt/",
-    review: {
-      "@type": "Review",
-      author: {
-        "@type": "Person",
-        name: "",
-      },
-    },
-    aggregateRating: {
-      "@type": "AggregateRating",
-      ratingValue: 0,
-      ratingCount: 0,
-      bestRating: 5,
-      worstRating: 1,
-    },
-  }
-
   const { dataJson: dynamicValues } = useStaticQuery(graphql`
     {
       dataJson {
         ratingValue
         reviewCount
-        aggregateRating
       }
     }
   `)
 
-  provenExpertData.aggregateRating.ratingValue = dynamicValues.ratingValue
-  provenExpertData.aggregateRating.ratingCount = dynamicValues.reviewCount
+  const rating = Number(dynamicValues?.ratingValue || 0)
+  const count = Number(dynamicValues?.reviewCount || 0)
+  if (!(rating > 0 && count > 0)) return null
+
+  const provenExpertData = {
+    "@context": "https://schema.org",
+    "@type": "Product",
+    "name": "LPE | Lars-Peter Eckhardt",
+    "description":
+      "Ihr Makler für: Altersvorsorge | Versicherungen | Finanzierungen | Immobilien",
+    "brand": { "@type": "Brand", "name": "LPE" },
+    "image":
+      "https://images.provenexpert.com/75/85/c0fdf5733a2767d5d7634b36fd88/lars-peter-eckhardt_full_1524080663.jpg",
+    "url": "https://www.lpe.de/",
+    "sameAs": ["https://www.provenexpert.com/lars-peter-eckhardt/"],
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": rating,
+      "ratingCount": count,
+      "bestRating": 5,
+      "worstRating": 1
+    }
+  }
 
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(provenExpertData) }}
-    ></script>
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(provenExpertData),
+      }}
+    />
   )
 }
 
